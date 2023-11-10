d4af50887f7121f5620c551f46cbf69f71a8ab32
Nixon Deb Antu <nixondebantu@gmail.com>
2023-11-10 10:09:37 +0600

    add Task_103

---
+++ Task_103.m
@@ -0,0 +1,34 @@
+%input
+f = input("Enter function: ");
+a = input("Enter left side of interval: ");
+b = input("Enter right side of interval: ");
+n = input("Enter no of iterations: ");
+e = input("Enter tolerance: ");
+
+%root storing matrix
+roots = [];
+
+%False-position start
+if f(a)*f(b)<0 && a<b
+    for i=1:n
+        c = (a*f(b) - b*f(a))/(f(b) - f(a));    %root caluculation
+        fprintf("Root for iteration %d is: %f\n",i,c);  %root printing
+        roots = [roots; c]; %root storing
+        if abs(f(c)) < e
+            break;
+        elseif f(a)*f(c) < 0
+            b = c;
+        else
+            a = c;
+        end
+    end
+
+else
+    fprintf("No root exists between the given intervals.");
+end
+
+%plotting
+fplot(f, [a,b]);   %plotting function
+hold on;
+plot(roots, f(roots), 'mo');    %plotting roots
+hold off;
